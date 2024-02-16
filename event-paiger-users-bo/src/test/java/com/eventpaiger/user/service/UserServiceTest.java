09f67425f452d2d9eaf764f7027e9809cfe41397
arturszmer <arturszmer@gmail.com>
2024-02-16 14:18:11 +0100

    A-003 - test github actions

--- event-paiger-users-bo/src/test/java/com/eventpaiger/user/service/UserServiceTest.java
+++ event-paiger-users-bo/src/test/java/com/eventpaiger/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserServiceTest {
 
@@ -19,7 +20,7 @@ class UserServiceTest {
     @Test
     public void should_return_false() {
         // given
-        assertFalse(userService.changePassword());
+        assertTrue(userService.changePassword());
 
         // when
 
